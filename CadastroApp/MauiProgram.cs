4a9e54cb39bf562ed2724e1619428790ca2000df
Lucas Alves Rocha <lucas.rocha@educacao.sp.gov.br>
2026-07-20 16:05:05 -0300

    Atualização Pacote MSIX

--- CadastroApp/MauiProgram.cs
+++ CadastroApp/MauiProgram.cs
@@ -1,7 +1,6 @@
 ﻿using CadastroApp.Services;
 using Microsoft.Extensions.Logging;
 using QuestPDF.Infrastructure;
-using SQLitePCL;
 
 namespace CadastroApp
 {
@@ -10,7 +9,7 @@ namespace CadastroApp
         public static MauiApp CreateMauiApp()
         {
 
-            Batteries_V2.Init();
+            
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
--- CadastroApp/Models/ViaCepResponse.cs
+++ CadastroApp/Models/ViaCepResponse.cs
@@ -8,7 +8,7 @@ namespace CadastroApp.Models
         public string Cep { get; set; } = string.Empty;
 
         [JsonPropertyName("bairro")]
-        public string Bairro { get; set; }
+        public string Bairro { get; set; } = string.Empty;
 
         [JsonPropertyName("logradouro")]
         public string Logradouro { get; set; } = string.Empty;
