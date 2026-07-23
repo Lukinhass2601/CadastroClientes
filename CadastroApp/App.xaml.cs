52a63f538e922c59af1e2b9e52bd5d0d5f6d5442
Lucas Alves Rocha <lucas.rocha@educacao.sp.gov.br>
2026-07-23 16:59:16 -0300

    Melhorias nas telas - Tamanho

--- CadastroApp/App.xaml.cs
+++ CadastroApp/App.xaml.cs
@@ -9,7 +9,22 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "CadastroApp" };
+            var window = new Window(new MainPage())
+            {
+                Title = "CadastroApp",
+
+                // Tamanho inicial usado em plataformas desktop
+                Width = 1500,
+                Height = 850,
+
+                // Impede que a janela fique pequena demais
+                MinimumWidth = 1100,
+                MinimumHeight = 700
+            };
+
+
+
+            return window;
         }
     }
 }
